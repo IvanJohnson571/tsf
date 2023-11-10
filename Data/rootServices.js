8231f13bec63f458881fe4c999db5c0f2c0ac866
Ivan <78429506+IvanJohnson571@users.noreply.github.com>
2023-11-10 14:00:41 +0200

    Add files via upload

---
+++ Data/rootServices.js
@@ -0,0 +1,33 @@
+const rootServices = [{
+  "_id": {
+    "$oid": "64072e6067488cb1719fb42b"
+  },
+  "url": "icon-3c.png",
+  "name": "Аудио Записи"
+}, {
+  "_id": {
+    "$oid": "64072e8767488cb1719fb42c"
+  },
+    "url": "icon-5c.png",
+  "name": "Репетиции"
+}, {
+  "_id": {
+    "$oid": "64072eb267488cb1719fb42d"
+  },
+    "url": "camera-2.png",
+  "name": "Видео"
+}, {
+  "_id": {
+    "$oid": "64072ecd67488cb1719fb42e"
+  },
+    "url": "icon-2c.png",
+  "name": "Мастъринг"
+}, {
+  "_id": {
+    "$oid": "64072eed67488cb1719fb42f"
+  },
+    "url": "icon-5c.png",
+  "name": "Миксинг"
+  }];
+
+module.exports = rootServices;
